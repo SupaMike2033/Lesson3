b3cc6cb51b4e3535473fc67146eaf46e70de3129
SupaMike2033 <nata_1477@mail.ru>
2021-02-05 01:05:17 +0300

    Calc commit

---
+++ app/src/main/java/com/example/lesson3/Calc/CalcMain.java
@@ -0,0 +1,31 @@
+package com.example.lesson3.Calc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lesson3.R;
+
+public class CalcMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calc_layout);
+
+        initView();
+    }
+
+    private void initView() {
+//        textCounter1 = findViewById(R.id.text1);
+//        textCounter2 = findViewById(R.id.text2);
+//        textCounter3 = findViewById(R.id.text3);
+//
+//        setButton1ClickListener();
+//        setButton2ClickListener();
+//        setButton3ClickListener();
+    }
+
+
+
+}
