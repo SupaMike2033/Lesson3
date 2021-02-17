2a391da679a0479a59a772977ac9692b881ba795
SupaMike2033 <nata_1477@mail.ru>
2021-02-17 22:27:27 +0300

    Some new themes added - commit

--- app/src/main/java/com/example/lesson3/Calc/Constants.java
+++ app/src/main/java/com/example/lesson3/Calc/Constants.java
@@ -3,9 +3,9 @@ package com.example.lesson3.Calc;
 import com.example.lesson3.R;
 
 public class Constants {
-    public static Integer[] images = {R.drawable.calc_regular, R.drawable.calc_pink};
-    public static Integer[] themeNames = {R.string.theme_regular, R.string.theme_pink};
-    public static Integer[] themeIDs = {R.style.AppTheme_CalcRegular, R.style.AppTheme_CalcPink};
+    public static Integer[] images = {R.drawable.calc_regular, R.drawable.calc_pink, R.drawable.calc_abs_black, R.drawable.calc_abs_black};
+    public static Integer[] themeNames = {R.string.theme_regular, R.string.theme_pink, R.string.theme_abs_black, R.string.theme_orange};
+    public static Integer[] themeIDs = {R.style.AppTheme_CalcRegular, R.style.AppTheme_CalcPink, R.style.AppTheme_CalcAbsBlack, R.style.AppTheme_CalcOrange};
 
     public static int currentTheme = 0;
     public static final String THEME_CHOOSE = "THEME_CHOOSE";
